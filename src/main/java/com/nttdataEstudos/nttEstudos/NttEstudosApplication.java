23ebcf65487acc7ce3d14355b023ecc4a0b116d2
Joao Esteves Santos <jestevsa@emeal.nttdata.com>
2023-05-08 21:22:34 -0300

    project created

---
+++ src/main/java/com/nttdataEstudos/nttEstudos/NttEstudosApplication.java
@@ -0,0 +1,13 @@
+package com.nttdataEstudos.nttEstudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttEstudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttEstudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdataEstudos/nttEstudos/NttEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdataEstudos.nttEstudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttEstudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
